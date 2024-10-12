9a4dde60d8140e1c39027ea334c1123d66a3cdad
RostyslavStan bc2024-3 <blekdgek2018@gmail.com>
2024-10-12 19:43:08 +0300

    Виконано 2 частину

--- index.js
+++ index.js
@@ -2,32 +2,41 @@ const { program } = require('commander');
 const fs = require('fs');
 
 program
-.requiredOption('-i, --input <file>')
-.option('-o, --output <file>')
-.option('-d, --display');
+  .requiredOption('-i, --input <file>')
+  .option('-o, --output <file>')
+  .option('-d, --display');
 
 program.parse();
 const options = program.opts();
 
-if(!fs.existsSync(options.input))
-{
-    console.log("Cannot find input file")
-} 
-
-const data = fs.readFileSync(options.input, 'utf8')
-
-if(options.output)
-{
-    fs.writeFile(options.output, data, 'utf8', (err) => 
-    {
-        if(err){
-            console.log(err);
-            return;
-        }
-    });
+if (!options.input) {
+  console.log("Please, specify input file");
 }
 
-if(options.display)
-    {
-        console.log(data);
-    }
+if (!fs.existsSync(options.input)) {
+  console.log("Cannot find input file");
+}
+
+const data = fs.readFileSync(options.input, 'utf8');
+const jsonData = JSON.parse(data);
+
+
+if (options.output) {
+  fs.writeFileSync(options.output, data, 'utf8');
+} 
+
+if (options.display) {
+  console.log(data);
+}
+
+let minElement = jsonData[0];
+
+jsonData.forEach(element => {
+  if (element.value < minElement.value) {
+    minElement = element;
+  }
+});
+
+const result = `${minElement.txt}:${minElement.value}`;
+
+fs.writeFileSync('output.txt', result, 'utf8');
